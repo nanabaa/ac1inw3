4f08c7c49770f19ba4badb46bad92efde548ccf0
unknown <anaclaranasc23@gmail.com>
2024-03-12 18:51:53 -0300

    faltando img e form

--- app.js
+++ app.js
@@ -14,9 +14,76 @@ function banner() {
     div.style.display = 'flex';
     div.style.alignItems = 'center';
     div.style.justifyContent = 'center';
-
-    window.sr = ScrollReveal({ reset: true });
-    sr.reveal('.banner', { duration: 4000 });
 }
 
 banner();
+
+const Equipe = () => {
+    let div = document.createElement("div");
+    div.setAttribute('class', 'equipe');
+    
+    var textoBanner = document.createElement("h1");
+    textoBanner.innerText = 'EQUIPES';
+
+    div.appendChild(textoBanner);
+    body.append(div);
+
+    div.style.display = 'flex';
+    div.style.alignItems = 'center';
+    div.style.justifyContent = 'center';
+
+}
+Equipe();
+
+const Habilidades = () => {
+    let div = document.createElement("div");
+    div.setAttribute('class', 'habilidades');
+    
+    var textoBanner = document.createElement("h1");
+    textoBanner.innerText = 'HABILIDADES E PROJETOS';
+
+    div.appendChild(textoBanner);
+    body.append(div);
+
+    div.style.display = 'flex';
+    div.style.alignItems = 'center';
+    div.style.justifyContent = 'center';
+}
+Habilidades();
+
+const HabilidadesIntro = () => {
+    let div = document.createElement("div");
+    div.setAttribute('class', 'habilidadesIntro');
+    
+    var textoBanner = document.createElement("h1");
+    textoBanner.innerText = 'A equipe atua em projetos que abrangem as áreas de design, prototipagem e front-end.';
+
+    div.appendChild(textoBanner);
+    body.append(div);
+
+    div.style.display = 'flex';
+    div.style.alignItems = 'center';
+    div.style.justifyContent = 'flex-start';
+}
+HabilidadesIntro();
+
+
+
+const Footer = () => {
+    let div = document.createElement("div");
+    div.setAttribute('class', 'footer');
+    
+    var textoBanner = document.createElement("h1");
+    textoBanner.innerText = 'CopyRight © 2024 | All Rights Reserved';
+
+    div.appendChild(textoBanner);
+    body.append(div);
+
+    div.style.display = 'flex';
+    div.style.alignItems = 'center';
+    div.style.justifyContent = 'center';
+
+
+}
+Footer();
+
